abcf0717a8f94da235a30c50b8549abeb85ac919
gmoulin <gmoulin.dev@gmail.com>
2013-01-10 11:05:04 +0100

    Blue Infinity modifications

---
+++ canvas-upload.php
@@ -0,0 +1,59 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+define( 'SWARM_ENTRY', 'API' );
+require_once 'inc/init.php';
+
+$browserInfo = $swarmContext->getBrowserInfo();
+$uaData = $browserInfo->getUaData();
+
+$request = $swarmContext->getRequest();
+
+$image = $request->getVal('imageData', null);
+$runID = $request->getInt( 'run_id', 0 );
+
+$response = array('success' => 0, 'filename' => '');
+
+if( !is_null($image) ){
+	$imgres = imagecreatefromstring( base64_decode(str_replace('data:image/png;base64,', '', $image)) );
+	if( $imgres != false ){
+		$db = $swarmContext->getDB();
+
+		echo '<pre>'; print_r($runID); echo '</pre>';
+
+		$row = $db->getRow(str_queryf('
+			SELECT
+				r.name as run_name, j.name as job_name
+			FROM runs r
+			INNER JOIN jobs j ON r.job_id = j.id
+			WHERE r.id = %u
+			',
+			$runID
+		));
+
+		echo '<pre>'; var_dump($row); echo '</pre>';
+
+		if( $row != false ){
+			$root = dirname(__FILE__).'/screenshots/';
+
+			if( !file_exists($root.$row->job_name) ){
+				mkdir($root.$row->job_name);
+			}
+
+			if( !file_exists($root.$row->run_name) ){
+				mkdir($root.$row->job_name.'/'.$row->run_name);
+			}
+
+			$filename = $row->job_name.'/'.$row->run_name.'/'.str_replace('/', ' - ', $uaData->displayInfo['title']).'.png';
+
+			imagepng($imgres, $root.$filename);
+
+			$response['success'] = 1;
+			$response['filename'] = $filename;
+		}
+	}
+}
+
+echo json_encode($response);
+die;
+?>
